abc6cdad4b30da59ec149fab040a656250150a2d
yhs4157 <yhs4157@naver.com>
2022-08-08 12:05:40 +0900

    test: 캐러셀을 위한 필요사항 점검

---
+++ src/carousel.js
@@ -0,0 +1,11 @@
+const nextBtn = document.createElement('button'); 
+const prevBtn = document.createElement('button'); 
+
+let count = 1; 
+
+nextBtn.onclick = () => {
+
+}
+
+const carousel = document.querySelector('#top-carousel');
+
